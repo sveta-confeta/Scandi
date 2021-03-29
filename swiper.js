57daf302fde985109a08f668cb86c3cc7722389d
sveta-confeta <_svet_@tut.by>
2021-03-29 15:30:43 +0300

    desctop

---
+++ swiper.js
@@ -0,0 +1,16 @@
+AOS.init();
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'fraction',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+$('button').on('click', function(){
+  $('.header_wrapper').toggleClass('open');
+});
